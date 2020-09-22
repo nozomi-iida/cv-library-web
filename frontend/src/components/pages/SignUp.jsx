14fc5423bbe8cec8126276e2c090c01f81463018
kei <xnkcelebrate48x@yahoo.co.jp>
2020-09-22 15:17:13 +0900

    [update] #7 機能の追加

--- frontend/src/components/pages/SignUp.jsx
+++ frontend/src/components/pages/SignUp.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState,useCallback } from 'react';
+import {useDropzone} from "react-dropzone"
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -12,6 +13,8 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import { useForm } from "react-hook-form";
+
 
 function Copyright() {
   return (
@@ -23,6 +26,35 @@ function Copyright() {
       {new Date().getFullYear()}
       {'.'}
     </Typography>
+  );
+}
+
+
+
+function MyDropzone() {
+  const onDrop = useCallback((acceptedFiles) => {
+    
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        const binaryStr = reader.result;
+        console.log(binaryStr);
+        
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  }, []);
+  const { getRootProps, getInputProps } = useDropzone({ onDrop });
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      <p>Drag 'n' drop some files here, or click to select files</p>
+    </div>
   );
 }
 
@@ -47,7 +79,17 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function SignUp() {
+  const [error,setError]=useState("")
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data) => {
+    {data.confirmpassword !==data.password && setError("passwordと一致しません")
+    
+    }
+    console.log(data);
+    }
   const classes = useStyles();
+
+  
 
   return (
     <Container component="main" maxWidth="xs">
@@ -59,54 +101,76 @@ export default function SignUp() {
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} onSubmit={handleSubmit(onSubmit)}>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
+            <Grid item xs={12}>
               <TextField
-                autoComplete="fname"
-                name="firstName"
+                autoComplete="name"
+                name="Name"
                 variant="outlined"
-                required
                 fullWidth
-                id="firstName"
-                label="First Name"
+                id="Name"
+                label="Name"
                 autoFocus
+                inputRef={register({ required: true })}
               />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-              />
+              {errors.Name && <span>This field is required</span>}
             </Grid>
             <Grid item xs={12}>
               <TextField
                 variant="outlined"
-                required
                 fullWidth
                 id="email"
                 label="Email Address"
                 name="email"
                 autoComplete="email"
+                inputRef={register({ required: true })}
+       
               />
+              {errors.email && <span>This field is required</span>}
             </Grid>
             <Grid item xs={12}>
               <TextField
                 variant="outlined"
-                required
                 fullWidth
                 name="password"
                 label="Password"
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                inputRef={register({ required: true })}
+
               />
+              {errors.password && <span>This field is required</span>}
             </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                fullWidth
+                name="confirmpassword"
+                label="confirmPassword"
+                type="password"
+                id="confirmpassword"
+                autoComplete="confirmpassword"
+                inputRef={register({ required: true })}
+
+              />
+                <span>{error}</span>
+              {errors.confirmpassword && <span>This field is required</span>}
+            </Grid>
+              
+            <MyDropzone onDrop={(acceptedFiles) => console.log(acceptedFiles)}>
+              {({ getRootProps, getInputProps }) => (
+                <section>
+                  <div {...getRootProps()}>
+                    <input {...getInputProps()} />
+                    <p>
+                      Drag 'n' drop some files here, or click to select files
+                    </p>
+                  </div>
+                </section>
+              )}
+            </MyDropzone>
             <Grid item xs={12}>
               <FormControlLabel
                 control={<Checkbox value="allowExtraEmails" color="primary" />}
@@ -114,6 +178,7 @@ export default function SignUp() {
               />
             </Grid>
           </Grid>
+
           <Button
             type="submit"
             fullWidth
