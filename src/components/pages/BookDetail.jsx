349a4fd269f8f3af793d7f9ec2cf3905120e7f1b
asagi <61268214+dev-mori@users.noreply.github.com>
2020-10-06 15:55:15 +0900

    [モーダルの作成 #27]

--- src/components/pages/BookDetail.jsx
+++ src/components/pages/BookDetail.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
+import DeleteModal from "../templates/DeleteModal";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -64,74 +65,82 @@ export default function Books({ history }) {
   const handleBack = () => {
     history.push("/");
   };
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
   return (
-    <div className={classes.root}>
-      <Button className={classes.back} onClick={handleBack}>
-        ←戻る
-      </Button>
-      <div className={classes.overview}>
-        <img
-          className={classes.img}
-          src={`${process.env.PUBLIC_URL}/reactimg.jpg`}
-          border="4"
-        />
-        <div className={classes.description}>
-          <h4 className={classes.text}>React.js&Next.js</h4>
-          <div className={classes.info}>
-            <p>作成者：イイダノゾミ</p>
-            <p className={classes.text}>作成日：2020/9/31</p>
+    <>
+      <div className={classes.root}>
+        <Button className={classes.back} onClick={handleBack}>
+          ←戻る
+        </Button>
+        <div className={classes.overview}>
+          <img
+            className={classes.img}
+            src={`${process.env.PUBLIC_URL}/reactimg.jpg`}
+            border="4"
+          />
+          <div className={classes.description}>
+            <h4 className={classes.text}>React.js&Next.js</h4>
+            <div className={classes.info}>
+              <p>作成者：イイダノゾミ</p>
+              <p className={classes.text}>作成日：2020/9/31</p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className={classes.details}>
-        <Button
-          className={classes.button}
-          variant="contained"
-          color="primary"
-          disableElevation
-        >
-          本を購入する
-        </Button>
-        <div className={classes.contents}>
-          <h3 className={classes.text}>本の簡単な概要</h3>
-          <p className={classes.text + " " + classes.sentence}>
-            サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
-          </p>
-          <div className={classes.more}>
-            <Button className={classes.read}>全文を読む</Button>
+        <div className={classes.details}>
+          <Button
+            className={classes.button}
+            variant="contained"
+            color="primary"
+            disableElevation
+          >
+            本を購入する
+          </Button>
+          <div className={classes.contents}>
+            <h3 className={classes.text}>本の簡単な概要</h3>
+            <p className={classes.text + " " + classes.sentence}>
+              サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
+            </p>
+            <div className={classes.more}>
+              <Button className={classes.read}>全文を読む</Button>
+            </div>
           </div>
+          <div className={classes.contents}>
+            <h3 className={classes.text}>読みたい理由</h3>
+            <p className={classes.text + " " + classes.sentence}>
+              サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
+            </p>
+          </div>
+          <Button
+            className={classes.button}
+            variant="contained"
+            color="primary"
+            disableElevation
+          >
+            編集する
+          </Button>
+          <Button
+            className={classes.button}
+            variant="contained"
+            color="primary"
+            disableElevation
+            onClick={handleOpen}
+          >
+            削除する
+          </Button>
+          <Button
+            className={classes.button}
+            variant="contained"
+            color="primary"
+            disableElevation
+          >
+            読了
+          </Button>
         </div>
-        <div className={classes.contents}>
-          <h3 className={classes.text}>読みたい理由</h3>
-          <p className={classes.text + " " + classes.sentence}>
-            サンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキストサンプルテキスト
-          </p>
-        </div>
-        <Button
-          className={classes.button}
-          variant="contained"
-          color="primary"
-          disableElevation
-        >
-          編集する
-        </Button>
-        <Button
-          className={classes.button}
-          variant="contained"
-          color="primary"
-          disableElevation
-        >
-          削除する
-        </Button>
-        <Button
-          className={classes.button}
-          variant="contained"
-          color="primary"
-          disableElevation
-        >
-          読了
-        </Button>
       </div>
-    </div>
+      <DeleteModal open={open} setOpen={setOpen} />
+    </>
   );
 }
