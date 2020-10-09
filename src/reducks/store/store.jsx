097ca3607bd9ada0ea1f5602e3afa0604eb98a0c
kei <xnkcelebrate48x@yahoo.co.jp>
2020-10-09 17:20:32 +0900

    [update]redux-devtoolsの導入

--- src/reducks/store/store.jsx
+++ src/reducks/store/store.jsx
@@ -11,6 +11,7 @@ export default function createStore() {
   return reduxCreateStore(
     combineReducers({
       books: BooksReducer,
-    })
+    }),
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 }
