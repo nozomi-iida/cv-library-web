f858a3e94f6899df45e122b5c33440009ebc39d2
kei <xnkcelebrate48x@yahoo.co.jp>
2020-10-12 17:32:13 +0900

    修正

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,13 @@ import BookDescription from "./components/pages/BookDescription";
 import EditForm from "./components/pages/EditForm";
 import { AuthProvider } from "./store/authStore";
 import LoggedInRoute from "./router/LoggedInRouter";
-import { useDispatch } from "react-redux";
+import { useDispatch,useSelector } from "react-redux";
 import firebase from "./firebase/firebase";
 import { BookAddAction } from "./reducks/books/actions";
 import ReadForm from "./components/pages/ReadForm";
 
 function App() {
+  const books =useSelector((state)=>state.books)
   const dispatch = useDispatch();
   useEffect(() => {
     firebase
@@ -44,12 +45,17 @@ function App() {
               path="/book/bookDitail/edit/:id"
               component={EditForm}
             />
+            {books.length&&(
               <Route exact path="/book/:id" component={BookDetail} />
+              )}
+              {books.length&&(
               <Route
-                exact
-                path="/book/:id/discription"
-                component={BookDescription}
+              exact
+              path="/book/:id/discription"
+              component={BookDescription}
               />
+              )}
+              
             <LoggedInRoute exact path="/add" component={Form} />
             <Route exact path="/signIn" component={SignIn} />
             <Route exact path="/" component={BookList} />
